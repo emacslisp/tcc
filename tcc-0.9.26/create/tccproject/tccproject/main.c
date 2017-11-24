1b67a625bdd8d0b1aaf855d206b61c9e2a2f1b52
emacslisp <wudi850620@hotmail.com>
2017-11-24 11:47:34 +1100

    try using xcode as ide of tcc

---
+++ tcc-0.9.26/create/tccproject/tccproject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tccproject
+//
+//  Created by eddy.wu on 11/24/17.
+//  Copyright © 2017 eddy.wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
